f216600c2d15ad97e2d82ca15f62616300cc28dc
EhrenW1 <ehren.wargo@patelhs.org>
2020-11-13 15:54:31 +0000

    added a string to print out the names

--- Main.java
+++ Main.java
@@ -37,7 +37,7 @@ class Main {
 
     String[ ] studentNames = {name1, name2, name3, name4, name5, name6, name7, name8, name9, name10, name11, name12, name13, name14, name15};
 
-     for(int i = 0; i < 15; i++){
+     for(int i = 0; i < 15; i++){ 
    System.out.println("The name of your student is " + studentNames[i]);
      }
  }
