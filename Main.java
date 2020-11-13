9ec4d66e1923883654e694b0f6672b408daa472c
EhrenW1 <ehren.wargo@patelhs.org>
2020-11-13 15:20:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+ 
+class Main {
+ public static void main(String[] args) {
+   Scanner scan = new Scanner(System.in);
+   //scanner
+   System.out.println("What is the name of your first student?");
+   String name = scan.next():
